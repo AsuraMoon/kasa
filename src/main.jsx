f890f5082ab0d2ffe64cef438d7f2eb2883988c2
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2023-08-30 12:14:59 +0200

    Récup autre Ordi (missing key)

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import "./index.css";
-import Root from "./routes/root"
-import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage/>,
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      }
-    ]
-  },
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
